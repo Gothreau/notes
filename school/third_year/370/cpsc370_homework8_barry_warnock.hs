2f725af111876871e17606127db9738043f244ad
Barry Warnock <brrwarnock@gmail.com>
2016-12-02 12:23:28 -0800

    340 class and 370 homwork

--- school/third_year/370/cpsc370_homework8_barry_warnock.hs
+++
@@ -1,6 +0,0 @@
-data LeafTree a = Leaf a | Branch (LeafTree a) (LeafTree a)
-
-removeIf :: (a -> Bool) -> LeafTree a -> Maybe (LeafTree a)
-removeIf f tree = Just tree
-
-main = display "hello world"
